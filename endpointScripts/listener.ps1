1036319cd88c5b69877245aa5651a9ee859bfc6d
Matthew Pearon <mpearon@users.noreply.github.com>
2020-05-17 17:47:24 -0500

    WIP: Added variable support

--- endpointScripts/listener.ps1
+++ endpointScripts/listener.ps1
@@ -5,30 +5,28 @@ $listener.UnsafeConnectionNtlmAuthentication = $true
 $listener.Start()
 
 while($listener.IsListening -eq $true){
-    $context = $listener.GetContext()
-    $request = $context.Request
-    $response = $context.Response
+	$context = $listener.GetContext()
+	
+	$requestedFunction = ($context.Request.RawUrl.split('?')[0]).replace('','')
+	$requestedVariable = ($context.Request.RawUrl.split('?')[1])
 
-    switch($request.RawUrl){
-        '/'                 { $html = 'Folding@Home Monitor' }
-        '/getSlotInfo'     { $html = ( FAHClient --send-command "slot-info" ) }
-        '/getQueueInfo'    { $html = ( FAHClient --send-command "queue-info" | Select-String -Pattern '{.+?}' | ConvertFrom-JSON | Out-String ) }
-        '/getPowerLevel'   { $html = ( (FAHClient --send-command "options power" | Select-String -Pattern '{.+?}' | ConvertFrom-JSON).power ) }
-        '/setLightPower'   { FAHClient --send-command "options power light"; $html = (FAHClient --send-command "options power" | Select-String -Pattern '{.+?}' | ConvertFrom-JSON).power }
-        '/setMediumPower'  { FAHClient --send-command "options power medium"; $html = (FAHClient --send-command "options power" | Select-String -Pattern '{.+?}' | ConvertFrom-JSON).power }
-        '/setHighPower'    { FAHClient --send-command "options power high"; $html = (FAHClient --send-command "options power" | Select-String -Pattern '{.+?}' | ConvertFrom-JSON).power }
-        '/getUser'         { $html = ( (FAHClient --send-command "options user" | Select-String -Pattern '{.+?}' | ConvertFrom-JSON).user ) }
-        '/getTeam'         { $html = ( (FAHClient --send-command "options team" | Select-String -Pattern '{.+?}' | ConvertFrom-JSON).team ) }
-        '/pause'           { FAHClient --send-command "pause *"; $html = (FAHClient --send-command "slot-info") }
-        '/unpause'         { FAHClient --send-command "unpause *"; $html = (FAHClient --send-command "slot-info") }
-        '/onIdle'          { FAHClient --send-command "on_idle *"; $html = (FAHClient --send-command "slot-info") }
-        '/finish'          { FAHClient --send-command "finish *"; $html = (FAHClient --send-command "slot-info") }
-        default             { $html = 'Unknown option' }
-    }
-    $buffer = [System.Text.Encoding]::UTF8.GetBytes($html)
-    $context.Response.ContentLength64 = $buffer.Length
-    $context.Response.OutputStream.Write($buffer, 0, $buffer.Length)
-    $context.Response.OutputStream.Close()
+	switch($requestedFunction){
+		'getSlotInfo'	{ $html = ( FAHClient --send-command "slot-info" ) }
+		'getQueueInfo'	{ $html = ( FAHClient --send-command "queue-info" | Select-String -Pattern '{.+?}' | ConvertFrom-JSON | Out-String ) }
+		'getPowerLevel'	{ $html = ( (FAHClient --send-command "options power" | Select-String -Pattern '{.+?}' | ConvertFrom-JSON).power ) }
+		'setPowerLevel'	{ FAHClient --send-command "options power $requestedVariable"; $html = (FAHClient --send-command "options power" | Select-String -Pattern '{.+?}' | ConvertFrom-JSON).power }
+		'getUser'		{ $html = ( (FAHClient --send-command "options user" | Select-String -Pattern '{.+?}' | ConvertFrom-JSON).user ) }
+		'getTeam'		{ $html = ( (FAHClient --send-command "options team" | Select-String -Pattern '{.+?}' | ConvertFrom-JSON).team ) }
+		'pause'			{ FAHClient --send-command "pause *"; $html = (FAHClient --send-command "slot-info") }
+		'unpause'		{ FAHClient --send-command "unpause *"; $html = (FAHClient --send-command "slot-info") }
+		'onIdle'		{ FAHClient --send-command "on_idle *"; $html = (FAHClient --send-command "slot-info") }
+		'finish'		{ FAHClient --send-command "finish *"; $html = (FAHClient --send-command "slot-info") }
+		default			{ $html = 'Unknown option' }
+	}
+	$buffer = [System.Text.Encoding]::UTF8.GetBytes($html)
+	$context.Response.ContentLength64 = $buffer.Length
+	$context.Response.OutputStream.Write($buffer, 0, $buffer.Length)
+	$context.Response.OutputStream.Close()
 }
 $listener.Stop()
 $listener.Close()
