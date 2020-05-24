971713a458d4e2b7a396557f6d2b7d605f9b6698
Matthew A. Pearon <mpearon@users.noreply.github.com>
2020-05-23 21:39:33 -0500

    WIP: PSScriptRoot, setFinish - Adjusted paths to be dynamic - Added provisions for setFinish endpoint

--- endpointScripts/listener.ps1
+++ endpointScripts/listener.ps1
@@ -17,33 +17,33 @@ while(($listener.IsListening -eq $true) -and ($stopServer -eq $false)){
 	}
 	switch($requestedFunction){
 		'slotInfo'	{
-			$return = pwsh -file ./Invoke-fahQuery.ps1 -commandText 'slot' -format $requestedFormat
+			$return = pwsh -file (-join($PSScriptRoot,'/Invoke-fahQuery.ps1')) -commandText 'slot' -format $requestedFormat
 		}
 		'queueInfo'	{
-			$return = pwsh -file ./Invoke-fahQuery.ps1 -commandText 'queue' -format $requestedFormat
+			$return = pwsh -file (-join($PSScriptRoot,'/Invoke-fahQuery.ps1')) -commandText 'queue' -format $requestedFormat
 		}
 		'powerInfo'	{
-			$return = pwsh -file ./Invoke-fahQuery.ps1 -commandText 'power' -format $requestedFormat
+			$return = pwsh -file (-join($PSScriptRoot,'/Invoke-fahQuery.ps1')) -commandText 'power' -format $requestedFormat
 		}
 		'userInfo'	{
-			$return = pwsh -file ./Invoke-fahQuery.ps1 -commandText 'user' -format $requestedFormat
+			$return = pwsh -file (-join($PSScriptRoot,'/Invoke-fahQuery.ps1')) -commandText 'user' -format $requestedFormat
 		}
 		'teamInfo'	{
-			$return = pwsh -file ./Invoke-fahQuery.ps1 -commandText 'team' -format $requestedFormat
+			$return = pwsh -file (-join($PSScriptRoot,'/Invoke-fahQuery.ps1')) -commandText 'team' -format $requestedFormat
 		}
 		'setPower'	{
 			switch($requestedVariable){
 				'full'	{
-					pwsh -file ./Invoke-fahCommand.ps1 -commandText 'setPower' -argument 'full'
-					$return = pwsh -file ./Invoke-fahQuery.ps1 -commandText 'power' -format $requestedFormat
+					pwsh -file (-join($PSScriptRoot,'/Invoke-fahCommand.ps1')) -commandText 'setPower' -argument 'full'
+					$return = pwsh -file (-join($PSScriptRoot,'/Invoke-fahQuery.ps1')) -commandText 'power' -format $requestedFormat
 				}
 				'medium'	{
-					pwsh -file ./Invoke-fahCommand.ps1 -commandText 'setPower' -argument 'medium'
-					$return = pwsh -file ./Invoke-fahQuery.ps1 -commandText 'power' -format $requestedFormat
+					pwsh -file (-join($PSScriptRoot,'/Invoke-fahCommand.ps1')) -commandText 'setPower' -argument 'medium'
+					$return = pwsh -file (-join($PSScriptRoot,'/Invoke-fahQuery.ps1')) -commandText 'power' -format $requestedFormat
 				}
 				'light'	{
-					pwsh -file ./Invoke-fahCommand.ps1 -commandText 'setPower' -argument 'light'
-					$return = pwsh -file ./Invoke-fahQuery.ps1 -commandText 'power' -format $requestedFormat
+					pwsh -file (-join($PSScriptRoot,'/Invoke-fahCommand.ps1')) -commandText 'setPower' -argument 'light'
+					$return = pwsh -file (-join($PSScriptRoot,'/Invoke-fahQuery.ps1')) -commandText 'power' -format $requestedFormat
 				}
 				default	{
 					$return = 'listener\setPower: Expected full|medium|light'
@@ -53,12 +53,12 @@ while(($listener.IsListening -eq $true) -and ($stopServer -eq $false)){
 		'setPause'	{
 			switch($requestedVariable){
 				$true	{
-					pwsh -file ./Invoke-fahCommand.ps1 -commandText 'pause'
-					$return = pwsh -file ./Invoke-fahQuery.ps1 -commandText 'slot' -format $requestedFormat
+					pwsh -file (-join($PSScriptRoot,'/Invoke-fahCommand.ps1')) -commandText 'pause'
+					$return = pwsh -file (-join($PSScriptRoot,'/Invoke-fahQuery.ps1')) -commandText 'slot' -format $requestedFormat
 				}
 				$false	{
-					pwsh -file ./Invoke-fahCommand.ps1 -commandText 'unpause'
-					$return = pwsh -file ./Invoke-fahQuery.ps1 -commandText 'slot' -format $requestedFormat
+					pwsh -file (-join($PSScriptRoot,'/Invoke-fahCommand.ps1')) -commandText 'unpause'
+					$return = pwsh -file (-join($PSScriptRoot,'/Invoke-fahQuery.ps1')) -commandText 'slot' -format $requestedFormat
 				}
 				default	{
 					$return = 'listener\setPause: Expected boolean'
@@ -68,17 +68,21 @@ while(($listener.IsListening -eq $true) -and ($stopServer -eq $false)){
 		'setOnIde'	{
 			switch($requestedVariable){
 				$true	{
-					pwsh -file ./Invoke-fahCommand.ps1 -commandText 'onIdle' -argument 'true'
-					$return =  pwsh -file ./Invoke-fahQuery.ps1 -commandText 'slot' -format $requestedFormat
+					pwsh -file (-join($PSScriptRoot,'/Invoke-fahCommand.ps1')) -commandText 'onIdle' -argument 'true'
+					$return =  pwsh -file (-join($PSScriptRoot,'/Invoke-fahQuery.ps1')) -commandText 'slot' -format $requestedFormat
 				}
 				$false	{
-					 pwsh -file ./Invoke-fahCommand.ps1 -commandText 'onIdle' -argument 'false'
-					 $return =  pwsh -file ./Invoke-fahQuery.ps1 -commandText 'slot' -format $requestedFormat
+					 pwsh -file (-join($PSScriptRoot,'/Invoke-fahCommand.ps1')) -commandText 'onIdle' -argument 'false'
+					 $return =  pwsh -file (-join($PSScriptRoot,'/Invoke-fahQuery.ps1')) -commandText 'slot' -format $requestedFormat
 				}
 				default	{
 					$return = 'listener\setOnIdle: Expected boolean'
 				}
 			}
+		}
+		'setFinish'	{
+			pwsh -file (-join($PSScriptRoot,'/Invoke-fahCommand.ps1')) -commandText 'finish' -argument 'false'
+			$return =  pwsh -file (-join($PSScriptRoot,'/Invoke-fahQuery.ps1')) -commandText 'slot' -format $requestedFormat
 		}
 		'stopServer'	{
 			$return = @"
@@ -103,13 +107,18 @@ while(($listener.IsListening -eq $true) -and ($stopServer -eq $false)){
 						<h1>Folding@Home Monitor</h1><br />
 						<font color="green">[RUNNING]</font>
 						<ul>
-							<li><a href="slotInfo">slotInfo</a></li>
-							<li>queueInfo</li>
-							<li>userInfo</li>
-							<li>teamInfo</li>
-							<li>setPower</li>
-							<li>setPause</li>
-							<li>setOnIdle</li>
+							<li><a href="slotInfo?format=html">slotInfo</a></li>
+							<li><a href="queueInfo?format=html">queueInfo</a></li>
+							<li><a href="userInfo">userInfo</a></li>
+							<li><a href="teamInfo">teamInfo</a></li>
+							<li><a href="setFinish">setFinish</a></li>
+							<li><a href="setPower?high">setPower - full</a></li>
+							<li><a href="setPower?medium">setPower - medium</a></li>
+							<li><a href="setPower?light">setPower - light</a></li>
+							<li><a href="setPause?true">setPause - true</a></li>
+							<li><a href="setPause?false">setPause - false</a></li>
+							<li><a href="setOnIdle?true">setOnIdle - true</a></li>
+							<li><a href="setOnIdle?false">setOnIdle - false</a></li>
 						</ul>
 					</body>
 				</html>
